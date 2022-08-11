44ed2c3c072b4374c256bab90b27f3916c789373
Adele <chindahadele2@gmail.com>
2022-08-12 01:49:52 +0300

    added abouts.map

--- frontend/src/containers/about/About.jsx
+++ frontend/src/containers/about/About.jsx
@@ -3,11 +3,34 @@ import motion from 'framer-motion';
 
 import './about.scss';
 
+const abouts =[
+  {title : "AI Development", description: "I am a certified web developer", imgUrl: ''},
+  {title : "Mobile Development", description: "I am a certified web developer", imgUrl: ''},
+  {title : "Backend", description: "I am a certified web developer, imgUrl: ''"},
+  {title : "UI/UX", description: "I am a certified web developer", imgUrl: ''},
+]; 
 const About = () => {
   return (
-    <div>
-        About
+    <>
+    <h2
+      className = 'head-text'>
+        I know that
+        <span>Good design</span>
+        <br/>
+        means
+        <span>Good business</span>
+    </h2>
+    <div className = "app__profiles">
+      {abouts.map((about, index) =>(
+        <motion.div
+          whileInView = {{opacity: 1}}
+          whileHover = {{scale: 1.1}}
+          transition = {{duration: 0.5, type: 'tween'}}
+          >
+        </motion.div>
+      ))}
     </div>
+    </>
   )
 }
 
