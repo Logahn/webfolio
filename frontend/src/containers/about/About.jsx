38366c6d6b5f13420936d03e6aa4ad27580c73ea
Adele <chindahadele2@gmail.com>
2022-08-13 01:58:40 +0300

    linked abouts with backend

--- frontend/src/containers/about/About.jsx
+++ frontend/src/containers/about/About.jsx
@@ -3,18 +3,26 @@ import { motion } from 'framer-motion';
 
 import './about.scss';
 import {images}from '../../constants';
+import {urlFor, client} from '../../client';
 
-const abouts =[
-  {title : "AI Development", description: "I am a certified web developer", imgUrl: images.about01},
-  {title : "Mobile Development", description: "I am a certified web developer", imgUrl: images.about02},
-  {title : "Backend Development", description: "I am a certified web developer", imgUrl: images.about03},
-  {title : "UI/UX", description: "I am a certified web developer", imgUrl: images.about04},
-  // {title : "Amazon Cloud Developer", description: "I am a certified web developer", imgUrl: images.amazon},
-  // {title : "Bolt Developer", description: "I am a certified web developer", imgUrl: images.bolt},
-
-
-]; 
+// const abouts =[
+//   {title : "AI Development", description: "I am a certified web developer", imgUrl: images.about01},
+//   {title : "Mobile Development", description: "I am a certified web developer", imgUrl: images.about02},
+//   {title : "Backend Development", description: "I am a certified web developer", imgUrl: images.about03},
+//   {title : "UI/UX", description: "I am a certified web developer", imgUrl: images.about04},
+//   // {title : "Amazon Cloud Developer", description: "I am a certified web developer", imgUrl: images.amazon},
+//   // {title : "Bolt Developer", description: "I am a certified web developer", imgUrl: images.bolt},
+// ]; 
 const About = () => {
+  const [abouts, setabouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query)
+    .then((data)=>{
+      setabouts(data);
+    })
+  }, []);
+  
   return (
     <>
     <h2
@@ -34,7 +42,7 @@ const About = () => {
           className = "app__profile-item"
           key = {about.title + index}
           >
-            <img src = {about.imgUrl} alt = {about.title}/>
+            <img src = {urlFor(about.imgUrl)} alt = {about.title}/>
             <h2 className = "bold-text" style={{marginTop: 20}}>{about.title}</h2>
             <p className = "p-text" style={{marginTop: 10}}>{about.description}</p>
 
