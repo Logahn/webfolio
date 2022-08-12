4e7fc0032b9194c1d2ebebc4a99555f10fc9f89e
Adele <chindahadele2@gmail.com>
2022-08-13 01:10:47 +0300

    sanity feature started

---
+++ frontend/src/client.js
@@ -0,0 +1,10 @@
+import sanityClient from  '@sanityClient';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = sanityClient({
+   projectId: '',
+   dataset: '',
+   apiVersion: '',
+   useCdn: '',
+   token: '',
+});
